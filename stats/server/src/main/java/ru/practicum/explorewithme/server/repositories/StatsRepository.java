48ba229e5aa801137a85feba9bf33471913a17fa
Ilya Remenchik <ilyaremenchik228@gmail.com>
2025-07-05 01:48:12 +0300

    tests success

--- stats/server/src/main/java/ru/practicum/explorewithme/server/repositories/StatsRepository.java
+++ stats/server/src/main/java/ru/practicum/explorewithme/server/repositories/StatsRepository.java
@@ -20,7 +20,7 @@ public interface StatsRepository extends JpaRepository<Hit, Integer> {
     @Query("SELECT new ru.practicum.explorewithme.dto.dtos.GetHitDto(h.app, h.uri, COUNT(h.ip)) FROM Hit h " +
             "WHERE h.time BETWEEN ?1 AND ?2 " +
             "GROUP BY h.app, h.uri " +
-            "ORDER BY COUNT(DISTINCT h.ip) DESC")
+            "ORDER BY COUNT(h.ip) DESC")
     List<GetHitDto> findByTimestamp(LocalDateTime from, LocalDateTime to);
 
     @Query("SELECT new ru.practicum.explorewithme.dto.dtos.GetHitDto(h.app, h.uri, COUNT(DISTINCT h.ip)) FROM Hit h " +
@@ -32,7 +32,7 @@ public interface StatsRepository extends JpaRepository<Hit, Integer> {
     @Query("SELECT new ru.practicum.explorewithme.dto.dtos.GetHitDto(h.app, h.uri, COUNT(h.ip)) FROM Hit h " +
             "WHERE h.time BETWEEN ?1 AND ?2 AND h.uri = ?3 " +
             "GROUP BY h.app, h.uri " +
-            "ORDER BY COUNT(DISTINCT h.ip) DESC")
+            "ORDER BY COUNT(h.ip) DESC")
     List<GetHitDto> findByTimestampAndUris(LocalDateTime from, LocalDateTime to, String uri);
 
 }
