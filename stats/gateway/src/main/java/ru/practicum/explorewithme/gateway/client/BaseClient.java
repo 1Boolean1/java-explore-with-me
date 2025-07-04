6f874c7f95daf873d7f0592a0fe1a71494af6f75
Ilya Remenchik <ilyaremenchik228@gmail.com>
2025-07-04 17:42:10 +0300

    refactor poms

--- stats/gateway/src/main/java/ru/practicum/explorewithme/gateway/client/BaseClient.java
+++ stats/gateway/src/main/java/ru/practicum/explorewithme/gateway/client/BaseClient.java
@@ -5,6 +5,8 @@ import org.springframework.lang.Nullable;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
+
+import org.springframework.http.HttpHeaders;
 import java.util.List;
 import java.util.Map;
 
