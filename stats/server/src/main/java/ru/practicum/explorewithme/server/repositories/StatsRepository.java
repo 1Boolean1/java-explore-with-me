6ae36d44e756cdc3268d43673f513a2fab91ddd1
Ilya Remenchik <ilyaremenchik228@gmail.com>
2025-07-07 12:08:23 +0300

    last refactor after review checkstyle

--- stats/server/src/main/java/ru/practicum/explorewithme/server/repositories/StatsRepository.java
+++ stats/server/src/main/java/ru/practicum/explorewithme/server/repositories/StatsRepository.java
@@ -2,7 +2,6 @@ package ru.practicum.explorewithme.server.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 import ru.practicum.explorewithme.dto.dtos.GetHitDto;
 import ru.practicum.explorewithme.server.models.Hit;
 
